beac116f4abd373915c4a963abe216843eef0812
Benterf123 <faithbenter@gmail.com>
2022-09-08 01:52:33 -0700

    mul.add

--- monty.h
+++ monty.h
@@ -91,6 +91,7 @@ char *_strtoky(char *s, char *d);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int _strcmp(char *s1, char *s2);
+global_t vglo;
 
 /* doubly linked list functions */
 stack_t *add_dnodeint_end(stack_t **head, const int n);
